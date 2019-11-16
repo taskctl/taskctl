1b288e329e8835d9235f4e3f23997dcce65dfcc5
Yevhen Terentiev <eugene@terentev.net>
2019-11-16 18:11:56 +0200

    Release 0.1.0-beta.4

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -28,7 +28,7 @@ func NewRootCommand() *cobra.Command {
 	loadConfig()
 	cmd := &cobra.Command{
 		Short:   "Wilson the task runner",
-		Version: "0.1.0-beta.3",
+		Version: "0.1.0-beta.4",
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
 			if debug {
 				log.SetLevel(log.DebugLevel)
--- wilson.rb
+++ wilson.rb
@@ -1,9 +1,9 @@
 class Wilson < Formula
-  version "0.1.0-beta.3"
+  version "0.1.0-beta.4"
   desc "Wilson the task runner"
   homepage "https://github.com/trntv/wilson"
   url "https://github.com/trntv/wilson/releases/download/#{version}/wilson-darwin-amd64.tar.gz"
-  sha256 "8c1a8ee623cd354d2df0f5750398372407dcf1ba1ca545c3b9a5d5d8a546cb9a"
+  sha256 "314dfbaf87186d56353f9049da88e791425c8e966f8763947d056b71adfa1bd3"
 
   def install
     bin.install "bin/wilson_darwin_amd64"
