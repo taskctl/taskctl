2d0afde1744e9e4514b3de3a03622066a4179b7e
Yevhen Terentiev <eugene@terentev.net>
2019-11-18 16:18:11 +0200

    Release 0.1.0

--- wilson.rb
+++ wilson.rb
@@ -1,9 +1,9 @@
 class Wilson < Formula
-  version "0.1.0-beta.4"
+  version "0.1.0"
   desc "Wilson the task runner"
   homepage "https://github.com/trntv/wilson"
   url "https://github.com/trntv/wilson/releases/download/#{version}/wilson-darwin-amd64.tar.gz"
-  sha256 "314dfbaf87186d56353f9049da88e791425c8e966f8763947d056b71adfa1bd3"
+  sha256 "69164670eedb95f6f13cc51f60b0ff6256594401217d73f168dd1aa47f859063"
 
   def install
     bin.install "bin/wilson_darwin_amd64"
