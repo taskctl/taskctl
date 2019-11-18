9246b3b061c3da1fd334622c1df5fb83eaf7e796
Yevhen Terentiev <eugene@terentev.net>
2019-11-18 22:53:41 +0200

    Fixes homebrew formula paths

--- wilson.rb
+++ wilson.rb
@@ -6,6 +6,6 @@ class Wilson < Formula
   sha256 "24defdc48268be9931779a5d15f59f20776725a205a32ec09bd0ed7a27ec542a"
 
   def install
-    bin.install "bin/wilson_darwin_amd64"
+    bin.install "wilson_darwin_amd64"
   end
 end
