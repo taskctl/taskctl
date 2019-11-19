4d28f406c43af7dbfc725fd571c7bf31a4daade2
Yevhen Terentiev <eugene@terentev.net>
2019-11-19 10:01:48 +0200

    Updates homebrew formula

--- wilson.rb
+++ wilson.rb
@@ -1,9 +1,9 @@
 class Wilson < Formula
-  version "0.1.1"
+  version "0.1.2"
   desc "Wilson - routine tasks automation toolkit"
   homepage "https://github.com/trntv/wilson"
   url "https://github.com/trntv/wilson/releases/download/#{version}/wilson-darwin-amd64.tar.gz"
-  sha256 "24defdc48268be9931779a5d15f59f20776725a205a32ec09bd0ed7a27ec542a"
+  sha256 "8535cd6be8ce93371739b5dc53b265ee05af474065e88dbd2067ad84386f056b"
 
   def install
     bin.install "wilson_darwin_amd64"
